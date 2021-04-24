b31db46717f64a13780bab3ac40225cea03b94e7
Jack Bennetto <jack@bennetto.com>
2021-04-23 21:55:42 -0700

    handle grand slam, print scores

--- game.py
+++ game.py
@@ -1,22 +1,23 @@
 import numpy as np
 
+
 class Board:
     def __init__(self, size=6):
         self.size = size
         self.initial = 4
         self.houses = np.full((2, size),
-                               fill_value=4,
-                               dtype=int)
-        self.score = np.zeros(2)
+                              fill_value=4,
+                              dtype=int)
+        self.score = np.zeros(2, dtype=int)
 
     def __str__(self):
         s = '\n'
-        s += '  ' + ' '.join(f'{x:2}' for x in self.houses[1, ::-1])
+        s += f' {self.score[1]:2}  ' + ' '.join(f'{x:2}' for x in self.houses[1, ::-1])
         s += '\n'
-        s += '  ' + ' '.join(f'{x:2}' for x in self.houses[0, :])
+        s += '     ' + ' '.join(f'{x:2}' for x in self.houses[0, :]) + f'  {self.score[0]:2}'
         s += '\n'
         return s
-    
+
     def move(self, player, house):
         save = self.houses.copy()
         seeds = self.houses[player, house]
@@ -28,7 +29,7 @@ class Board:
                 current_house = 0
                 current_side = 1 - current_side
             if current_house == house and current_side == player:
-                    continue
+                continue
             self.houses[current_side, current_house] += 1
             seeds -= 1
             current_house += 1
@@ -37,20 +38,20 @@ class Board:
             self.capture(current_side, current_house-1)
 
     def capture(self, side, house):
+        # check for grand slam
+        if (house == self.size and np.all((self.houses[side] == 2) |
+                                           self.houses[side] == 3)):
+            return
         current_house = house
         while (self.houses[side, current_house] in (2, 3) and
                current_house >= 0):
 
-            self.score[1-side] += self.houses[side, house] 
-            self.houses[side, house]  = 0
+            self.score[1-side] += self.houses[side, house]
+            self.houses[side, house] = 0
             current_house -= 1
 
 
-        
-
-
 if __name__ == '__main__':
-
     b = Board()
     print(b)
     b.move(0, 2)
