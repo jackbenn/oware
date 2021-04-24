d876f2e82b6143b71c0a2a5d599c5c57445a5667
Jack Bennetto <jack@bennetto.com>
2021-04-24 09:37:18 -0700

    update readme, check done code, put current player in board

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# mancala
+# Oware
+
+The beginnings of a a program to play [Oware](https://en.wikipedia.org/wiki/Oware) (a sowing game related to [Mancala](https://en.wikipedia.org/wiki/Mancala)) using reinforcement learning.
--- src/game.py
+++ src/game.py
@@ -5,37 +5,60 @@ class Board:
     def __init__(self, size=6):
         self.size = size
         self.initial = 4
+        self.to_win = self.size * self.initial + 1
         self.houses = np.full((2, size),
                               fill_value=4,
                               dtype=int)
         self.score = np.zeros(2, dtype=int)
+        self.player = 0
+        self.done = False
+        self.winner = 0.5
 
     def __str__(self):
         s = '\n'
-        s += f' {self.score[1]:2}  ' + ' '.join(f'{x:2}' for x in self.houses[1, ::-1])
+        s += f' {self.score[1]:2}  '
+        s += ''.join(f'({x:2} )' for x in self.houses[1, ::-1])
         s += '\n'
-        s += '     ' + ' '.join(f'{x:2}' for x in self.houses[0, :]) + f'  {self.score[0]:2}'
+        s += '     ' + ' --- ' * self.size + '\n'
+        s += '     '
+        s += ''.join(f'({x:2} )' for x in self.houses[0, :])
+        s += f'  {self.score[0]:2}'
         s += '\n'
         return s
 
-    def move(self, player, house):
+    def check_done(self):
+        if self.score[0] >= self.to_win:
+            self.done = True
+            self.winner = 0
+        elif self.score[1] >= self.to_win:
+            self.done = True
+            self.winner = 0
+        elif self.score[0] == self.score[1] == self.to_win - 1:
+            self.done = True
+            self.winner = 0.5
+        return self.done
+
+
+    def move(self, house):
         save = self.houses.copy()
-        seeds = self.houses[player, house]
-        self.houses[player, house] = 0
+        seeds = self.houses[self.player, house]
+        self.houses[self.player, house] = 0
         current_house = house + 1
-        current_side = player
+        current_side = self.player
         while seeds > 0:
             if current_house == self.size:
                 current_house = 0
                 current_side = 1 - current_side
-            if current_house == house and current_side == player:
+            if current_house == house and current_side == self.player:
                 continue
             self.houses[current_side, current_house] += 1
             seeds -= 1
             current_house += 1
 
-        if current_side != player:
+        if current_side != self.player:
             self.capture(current_side, current_house-1)
+        
+        self.player = 1 - self.player
 
     def capture(self, side, house):
         # check for grand slam
@@ -54,14 +77,14 @@ class Board:
 if __name__ == '__main__':
     b = Board()
     print(b)
-    b.move(0, 2)
+    b.move(2)
     print(b)
 
-    b.move(1, 5)
+    b.move(5)
     print(b)
 
-    b.move(0, 1)
+    b.move(1)
     print(b)
 
-    b.move(1, 4)
+    b.move(4)
     print(b)
