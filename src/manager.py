3783e4e181c32c664b5d38cf408a0f87198be3b9
Jack Bennetto <jack@bennetto.com>
2021-04-27 15:47:05 -0700

    add more control logic to manager

--- src/manager.py
+++ src/manager.py
@@ -11,12 +11,25 @@ class Manager:
         game = Game()
         player = 0
         state = game.state(player)
-        
+        rewards = []
+        states = []
+        states.append(game.state)
+        rewards.append(agent[0].move(state[-2], 0, states[-1]))
+
+        while True:
+            states.append(game.state)
+            reward = rewards[-1] - rewards[-2]
+            rewards.append(agent[1].move(state,
+                                        states[-1]))
+
     
     def load_agents(self):
         '''load agent weights from file'''
-        pass
+        agent[0].load('agent0')
+        agent[1].load('agent1')
+
 
     def save_agents(self):
         '''save agent weights to file'''
-        pass
+        agent[0].save('agent0')
+        agent[1].save('agent1')
